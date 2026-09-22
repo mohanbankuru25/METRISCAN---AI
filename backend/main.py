29289184bc5b58f8b8e4dd674cd47928088ef717
mohanbankuru25 <mohanbankuru25@gmail.com>
2026-09-22 12:11:20 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -35,6 +35,7 @@ app.add_middleware(
         "http://localhost:5174",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174",
+        "https://metriscan-ai-omega.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
